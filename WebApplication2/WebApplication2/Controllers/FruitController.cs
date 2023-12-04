53dd3251b3341593baf8a2c013fe04f9281c9536
Gyandip12202339 <12202339@students.koi.edu.au>
2023-12-05 10:21:51 +1100

    Uploading the file

---
+++ WebApplication2/WebApplication2/Controllers/FruitController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class FruitController : Controlle
+    {
+        // GET: Fruit
+        public ActionResult Index()
+        {
+            List<string> fruit = new List<string>();
+            fruit.Add("Mango");
+            fruit.Add("Apple");
+           fruit.Add("Banana");
+
+            ViewBag.store  = "Jackma Veggie";
+
+            ViewBag.Name = "my favorite fruit";
+            ViewBag.Fruit = fruit;
+
+
+
+
+            return View();
+        }
+    }
+}
